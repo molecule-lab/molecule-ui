9dff92b06f1a3f8041481458cc15abfe3410bf2c
molecule-lab-rushil <rushildhinoja@moleculelab.in>
2025-12-21 15:54:53 +0530

    perf: add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { ReactNode } from "react"
 import { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import { absoluteUrl, constructMetadata } from "@/lib/utils"
 import { ThemeProvider } from "@/components/theme-provider"
@@ -25,6 +26,7 @@ export default function Layout({ children }: { children: ReactNode }) {
       <body className="bg-background relative flex w-full flex-col justify-center overflow-x-hidden scroll-smooth font-sans antialiased">
         <ThemeProvider>{children}</ThemeProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
